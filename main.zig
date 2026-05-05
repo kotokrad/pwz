dfe81c65e933f41f9c530aada705a2c899371ea4
kotokrad <dsl2407@gmail.com>
2026-05-05 21:44:06 +0700

    Use Io.Group for spawning threads

--- main.zig
+++ main.zig
@@ -6,24 +6,22 @@ const print = std.debug.print;
 const session = @import("server/session.zig");
 
 pub fn main(init: std.process.Init) !void {
-    const io = init.io;
-    var gpa: std.heap.DebugAllocator(.{}) = .init;
-    defer _ = gpa.deinit();
+    const gpa = init.gpa;
+
+    var threaded: Io.Threaded = .init(gpa, .{});
+    const io = threaded.io();
 
     const address = try net.IpAddress.parse("127.0.0.1", 29000);
     var server = try address.listen(io, .{ .reuse_address = true });
     print("INFO: Listening on {f}\n", .{address});
     defer server.deinit(io);
 
+    var group: Io.Group = .init;
+    defer group.cancel(io);
+
     while (true) {
         print("INFO: Waiting for new connection...\n", .{});
         const stream = try server.accept(io);
-        _ = io.async(session.start, .{ io, gpa.allocator(), stream });
-
-        // var task = io.async(session.start, .{ io, stream });
-        // defer _ = task.cancel(io);
-
-        // const thread = try std.Thread.spawn(.{}, session.run, .{ io, stream });
-        // thread.detach();
+        try group.concurrent(io, session.start, .{ io, gpa, stream });
     }
 }
