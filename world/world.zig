e43dab81ff4ea2306e2811987108e0bac3e75bb6
kotokrad <dsl2407@gmail.com>
2026-05-07 03:49:32 +0700

    Refactor data storage

--- world/world.zig
+++ world/world.zig
@@ -30,10 +30,48 @@ pub const Account = struct {
     chars: CharIds,
 };
 
-const Accounts = std.StringHashMap(Account);
-const Characters = [256]Character;
+const DB = struct {
+    accounts: std.StringHashMap(Account),
+    characters: [256]Character,
+
+    fn init(gpa: std.mem.Allocator) DB {
+        return .{
+            .accounts = .init(gpa),
+            .characters = undefined,
+        };
+    }
+
+    pub fn seed_with_test_data(self: *DB, gpa: std.mem.Allocator) !void {
+        var char_ids: [8]u8 = undefined;
+        char_ids[0] = 123;
+        const test_account: Account = .{
+            .id = 35,
+            .hash = .{ 41, 24, 244, 86, 253, 162, 124, 112, 60, 169, 226, 90, 244, 110, 139, 36 }, // qwer:qwer
+            .chars = .{ .chars = char_ids, .len = 1 },
+        };
+        try self.accounts.put("qwer", test_account);
+
+        self.characters[123] = try character.getExampleChar(gpa);
+    }
+
+    fn getCharacterList(self: *DB, ids: CharIds) CharList {
+        var char_list: CharList = .{
+            .chars = undefined,
+            .len = ids.len,
+        };
+        for (ids.chars[0..ids.len], 0..) |id, i| {
+            char_list.chars[i] = self.characters[id];
+        }
+        return char_list;
+    }
+
+    pub fn deinit(self: *DB) void {
+        self.accounts.deinit();
+    }
+};
+
 const World = struct {
-    chars: [256]Character,
+    players_online: [256]bool,
     sessions: [256]*Channel(Update),
     next_session_id: u8,
 
@@ -43,31 +81,7 @@ const World = struct {
         self.next_session_id += 1;
         return id;
     }
-
-    fn getCharacterList(self: *World, ids: CharIds) CharList {
-        var char_list: CharList = .{
-            .chars = undefined,
-            .len = ids.len,
-        };
-        for (ids.chars[0..ids.len], 0..) |id, i| {
-            char_list.chars[i] = self.chars[id];
-        }
-        return char_list;
-    }
 };
-
-pub fn seed_with_test_data(gpa: std.mem.Allocator, accounts: *Accounts, world: *World) !void {
-    var char_ids: [8]u8 = undefined;
-    char_ids[0] = 123;
-    const test_account: Account = .{
-        .id = 35,
-        .hash = .{ 41, 24, 244, 86, 253, 162, 124, 112, 60, 169, 226, 90, 244, 110, 139, 36 }, // qwer:qwer
-        .chars = .{ .chars = char_ids, .len = 1 },
-    };
-    try accounts.put("qwer", test_account);
-
-    world.chars[123] = try character.getExampleChar(gpa);
-}
 
 pub fn start(io: std.Io, gpa: std.mem.Allocator, actions_rx: *Channel(Action)) void {
     loop(io, gpa, actions_rx) catch |err| print("ERROR: World process crashed with error: {}", .{err});
@@ -75,21 +89,20 @@ pub fn start(io: std.Io, gpa: std.mem.Allocator, actions_rx: *Channel(Action)) v
 
 pub fn loop(io: std.Io, gpa: std.mem.Allocator, actions_rx: *Channel(Action)) !void {
     var world: World = .{
-        .chars = undefined,
+        .players_online = undefined,
         .sessions = undefined,
         .next_session_id = 0,
     };
 
     // Here for now
-    var accounts: Accounts = .init(gpa);
-    defer accounts.deinit();
-
-    try seed_with_test_data(gpa, &accounts, &world);
+    var db: DB = .init(gpa);
+    defer db.deinit();
+    try db.seed_with_test_data(gpa);
 
     while (true) {
         for (try actions_rx.drain()) |action| {
             switch (action) {
-                .auth => |payload| payload.reply.set(io, accounts.get(payload.username)),
+                .auth => |payload| payload.reply.set(io, db.accounts.get(payload.username)),
                 .init_session => |payload| {
                     const session_id = world.registerSession(payload.channel);
                     payload.reply.set(io, session_id);
@@ -98,7 +111,7 @@ pub fn loop(io: std.Io, gpa: std.mem.Allocator, actions_rx: *Channel(Action)) !v
                     var arena: std.heap.ArenaAllocator = .init(gpa);
                     errdefer arena.deinit();
                     const allocator = arena.allocator();
-                    const char_list = world.getCharacterList(payload.ids);
+                    const char_list = db.getCharacterList(payload.ids);
                     const result = try allocator.alloc(types.RoleInfo, payload.ids.len);
                     for (char_list.chars[0..char_list.len], 0..) |char, i| {
                         result[i] = try types.RoleInfo.from(allocator, char);
@@ -106,7 +119,7 @@ pub fn loop(io: std.Io, gpa: std.mem.Allocator, actions_rx: *Channel(Action)) !v
                     payload.reply.set(io, .{ .arena = arena, .value = result });
                 },
                 .enter_world => |payload| {
-                    _ = payload;
+                    world.players_online[payload.char_id] = true;
                 },
                 .move => |payload| {
                     _ = payload;
